cabc055bf6050cd914f08fc423545872a0c0590c
hakiki19 <mdl.hqq@gmail.com>
2020-04-06 10:38:20 +0700

    commit pertama

--- DiskonBertingkat/Program.cs
+++ DiskonBertingkat/Program.cs
@@ -10,6 +10,17 @@ namespace DiskonBertingkat
     {
         static void Main(string[] args)
         {
-        }
+            Console.Write("input tingkat diskon :");
+            int nDiskon = Convert.ToInt32(Console.ReadLine());
+            int output =100000 ;
+            for (int i = 0; i < nDiskon; i++)
+            {
+                Console.Write("Diskon tingkat {0} : ", i);
+                int diskon = Convert.ToInt32(Console.ReadLine().ToString());
+                output -= (output * diskon) / 100;
+            }
+            Console.WriteLine("Output {0}",output);
+            Console.ReadKey();
+        }        
     }
 }
